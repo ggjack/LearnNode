5856f9a7c9294db397677d215964a0d68ae6ebb0
Jack Chen <gg.jack@hotmail.com>
2017-01-13 04:12:33 +0000

    Added database to store campgrounds

--- yelpCamp/app.js
+++ yelpCamp/app.js
@@ -1,17 +1,32 @@
-var express = require("express");
-var app= express();
-var bodyParser= require("body-parser");
-var campgrounds = [{name : "M", image: "http://vignette4.wikia.nocookie.net/pokemon/images/3/32/258Mudkip_AG_anime_2.png/revision/latest?cb=20150102063741"},
-{name : "M", image: "http://vignette4.wikia.nocookie.net/pokemon/images/3/32/258Mudkip_AG_anime_2.png/revision/latest?cb=20150102063741"},
-{name : "U", image: "http://vignette4.wikia.nocookie.net/pokemon/images/3/32/258Mudkip_AG_anime_2.png/revision/latest?cb=20150102063741"},
-{name : "D", image: "http://vignette4.wikia.nocookie.net/pokemon/images/3/32/258Mudkip_AG_anime_2.png/revision/latest?cb=20150102063741"},
-{name : "K", image: "http://vignette4.wikia.nocookie.net/pokemon/images/3/32/258Mudkip_AG_anime_2.png/revision/latest?cb=20150102063741"},
-{name : "I", image: "http://vignette4.wikia.nocookie.net/pokemon/images/3/32/258Mudkip_AG_anime_2.png/revision/latest?cb=20150102063741"},
-{name : "P", image: "http://vignette4.wikia.nocookie.net/pokemon/images/3/32/258Mudkip_AG_anime_2.png/revision/latest?cb=20150102063741"}];
+var express = require("express"),
+    app = express(),
+    bodyParser = require("body-parser"),
+    mongoose = require("mongoose");
 
-
+mongoose.connect("mongodb://localhost/yelp_camp");
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
+
+//SCHEMA SETUP
+var campgroundSchema = new mongoose.Schema({
+    name: String,
+    image: String
+});
+
+var Campground = mongoose.model("Campground", campgroundSchema);
+
+// Campground.create({
+//     name: "Granite Hill",
+//     image : "https://ladyisaglamp.files.wordpress.com/2014/07/iphone-download-2014-07-27-069-e1406507217572.jpg"
+// },function(err,campground){
+//     if(err){
+//         console.log(err);
+//     }else{
+//         console.log("Newly created Campground!");
+//         console.log(campground);
+//     }
+// });
+
 
 app.get("/", function(req,res){
     //res.send("This is just a placeholder"); 
@@ -20,7 +35,14 @@ app.get("/", function(req,res){
 });
 
 app.get("/campgrounds", function(req, res){
-    res.render("campgrounds", {campgrounds: campgrounds});
+    //Get all campground from DB
+    Campground.find({},function(err,allCampgrounds){
+        if(err){
+            console.log(err);
+        }else{
+            res.render("campgrounds", {campgrounds: allCampgrounds});
+        }
+    });
 });
 
 app.post("/campgrounds", function(req,res){
@@ -30,10 +52,15 @@ app.post("/campgrounds", function(req,res){
     var name = req.body.name;
     var image = req.body.image;
     var newCampground= {name: name, image: image};
-    campgrounds.push(newCampground);
+    //Create a new Campground and save to database
+    Campground.create(newCampground, function(err, newlyCreated){
+        if(err){
+            console.log(err);
+        }else{
+            res.redirect("/campgrounds");
+        }
+    });
     //redirect back to campground page
-    res.redirect("/campgrounds");
-    
 });
 
 app.get("/campgrounds/new", function(req,res){
