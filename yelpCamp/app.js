2b6e1d86ee51fb33d77f521f54079b6f3136994a
Jack Chen <gg.jack@hotmail.com>
2017-01-14 23:38:18 +0000

    Added POST, and tidied up

--- yelpCamp/app.js
+++ yelpCamp/app.js
@@ -10,14 +10,16 @@ app.set("view engine", "ejs");
 //SCHEMA SETUP
 var campgroundSchema = new mongoose.Schema({
     name: String,
-    image: String
+    image: String,
+    description: String
 });
 
 var Campground = mongoose.model("Campground", campgroundSchema);
 
 // Campground.create({
 //     name: "Granite Hill",
-//     image : "https://ladyisaglamp.files.wordpress.com/2014/07/iphone-download-2014-07-27-069-e1406507217572.jpg"
+//     image : "https://ladyisaglamp.files.wordpress.com/2014/07/iphone-download-2014-07-27-069-e1406507217572.jpg",
+//     description: "This is a huge granite hill, no bathrooms, no water, and beautiful granite!"
 // },function(err,campground){
 //     if(err){
 //         console.log(err);
@@ -40,7 +42,7 @@ app.get("/campgrounds", function(req, res){
         if(err){
             console.log(err);
         }else{
-            res.render("campgrounds", {campgrounds: allCampgrounds});
+            res.render("index", {campgrounds: allCampgrounds});
         }
     });
 });
@@ -51,7 +53,8 @@ app.post("/campgrounds", function(req,res){
     
     var name = req.body.name;
     var image = req.body.image;
-    var newCampground= {name: name, image: image};
+    var desc = req.body.description;
+    var newCampground= {name: name, image: image, description: desc};
     //Create a new Campground and save to database
     Campground.create(newCampground, function(err, newlyCreated){
         if(err){
@@ -65,6 +68,17 @@ app.post("/campgrounds", function(req,res){
 
 app.get("/campgrounds/new", function(req,res){
     res.render("new");
+});
+
+app.get("/campgrounds/:id", function(req, res){
+    //find campground with provided  ID
+    Campground.findById(req.params.id, function(err, foundCampground){
+       if(err){
+           console.log(err);
+        } else{
+            res.render("show", {campground : foundCampground});     
+        }
+    });
 });
 
 app.listen(process.env.PORT, process.env.IP, function(){
